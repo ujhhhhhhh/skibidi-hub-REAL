aef4baca672950c660c851df3259a0780d4f3e0e
ujhhhhhhh <vividrbx27@gmail.com>
2025-06-30 19:31:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,6 @@ import importlib.util
 import os
 import stat
 
-def make_everything_777(root_dir="."):
-    mode = stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO  # 0o777
-    for dirpath, dirnames, filenames in os.walk(root_dir):
-        os.chmod(dirpath, mode)
-        for filename in filenames:
-            filepath = os.path.join(dirpath, filename)
-            try:
-                os.chmod(filepath, mode)
-            except PermissionError:
-                print(f"Permission denied for {filepath}")
-    print(f"All permissions set to 777 under {os.path.abspath(root_dir)}")
-
-make_everything_777()
 def ensure_flask_installed():
     if importlib.util.find_spec("flask") is None:
         print("Flask not found. Installing...")
